4ce591101c2279cba24b2925211b0e1d17714af8
sergey <sergeysereko@gmail.com>
2023-12-09 19:10:37 +0200

    add test Human

--- AbstractClassesInterfaces/src/Main.java
+++ AbstractClassesInterfaces/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        Human human1 = new Human();
+        Human human2 = new Human("Игорь",35,"Муж.");
+        System.out.print("Введите имя: ");
+        String name = scanner.nextLine();
+        System.out.print("Введите возраст: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Введите пол: ");
+        String gender = scanner.nextLine();
+        Human human3 = new Human(name,age,gender);
+        human1.outputHuman();
+        human2.outputHuman();
+        human3.outputHuman();
+
     }
+
 }
