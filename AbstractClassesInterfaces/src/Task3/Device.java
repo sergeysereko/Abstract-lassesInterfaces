71489b81582d547ec2d452b9d06fa59ba8edf117
sergey <sergeysereko@gmail.com>
2023-12-10 14:09:41 +0200

    add class Device

---
+++ AbstractClassesInterfaces/src/Task3/Device.java
@@ -0,0 +1,38 @@
+package Task3;
+public class Device {
+    private String name;
+
+    public Device(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+
+    public void Sound() {
+        System.out.println("Устройство издает какой-то звук");
+    }
+
+    public void Show() {
+        System.out.println("Название устройства: " + name);
+    }
+
+    public void Desc() {
+        System.out.println("Описание устройства: Общее устройство");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
