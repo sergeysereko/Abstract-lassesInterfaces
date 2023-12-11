5fe24b5604e2fa63e033eddb79edd8dac97a9643
sergey <sergeysereko@gmail.com>
2023-12-11 22:58:08 +0200

    add class Voilin

---
+++ AbstractClassesInterfaces/src/Task5/Violin.java
@@ -0,0 +1,45 @@
+package Task5;
+
+public class Violin extends MusicalInstrument{
+    private String typeViolin;
+
+    public Violin(String name, String typeViolin) {
+        super(name);
+        this.typeViolin = typeViolin;
+    }
+
+    @Override
+    public void Desc() {
+        System.out.println("Описание скрипки " + getName() + ": Тип - " + typeViolin);
+    }
+
+    @Override
+    public void History() {
+        System.out.println("История создания скрипки: Древнейший струнный инструмент");
+    }
+
+    @Override
+    public void Sound() {
+        System.out.println("Издает характерный звук скрипки");
+    }
+
+    @Override
+    public void Show() {
+        System.out.println("Название скрипки " + this.typeViolin);
+    }
+
+    @Override
+    public String toString() {
+        return "Violin{" +
+                "typeViolin='" + typeViolin + '\'' +
+                '}';
+    }
+
+    public String getTypeViolin() {
+        return typeViolin;
+    }
+
+    public void setTypeViolin(String typeViolin) {
+        this.typeViolin = typeViolin;
+    }
+}
